996fe0eba4930c90a85bc22e517b539bf1a027dd
cdhcsh <cdhcsh@gmail.com>
2022-11-27 01:01:55 +0900

    2022.11.27 springmvcV1 study (requestMapping)

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(Map<String,String> paramMap){
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", memberRepository.findAll());
+        return mv;
+    }
+}
